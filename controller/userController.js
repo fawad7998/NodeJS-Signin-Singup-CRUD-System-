08ee7e78eba9610e707a4a759cb9dbc27ae14f73
Fawad Ahmad <fawad7998@gmail.com>
2024-10-09 12:36:37 +0500

    CODE Updated

--- controller/userController.js
+++ controller/userController.js
@@ -1,22 +1,33 @@
-const User = require('../model/userSchema');
+const schema = require('../model/userSchema');
 const bcrypt = require('bcryptjs');
 
 const Register = async (req, res) => {
   console.log('Register endpoint hit');
   const { email, password, name } = req.body;
+  if (!email || !password || !name) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
 
   try {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await schema.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ error: 'Email already exists' });
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({
+    console.log({
       email,
       name,
       password: hashPassword,
     });
+    const newUser = await schema.create({
+      email,
+      name,
+      password: hashPassword,
+    });
+
+    console.log(hashPassword);
+
     res.status(201).json({
       newUser,
       message: 'User Created Successfully',
@@ -31,7 +42,7 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ email });
+    const user = await schema.findOne({ email });
     if (!user) return res.status(404).json({ error: 'User not found!' });
 
     const isMatch = await bcrypt.compare(password, user.password);
